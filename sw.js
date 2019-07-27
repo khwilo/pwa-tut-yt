99fa9c56ed2a8d325582eb395d9e447aee7f3983
khwilo <khwilowatai@gmail.com>
2019-07-27 15:17:31 +0300

    Listen to the activate event

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener("install", event => {
     console.log(`Service worker has been installed`);
 });
+
+// activate service worker
+self.addEventListener("activate", event => {
+    console.log(`Service worker has been activated`);
+});
